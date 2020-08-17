1fea71333a3b2d15e333be9a3e0d974a875792ee
Jackson Isenberg <jisenberg1050@gmail.com>
2020-08-17 11:21:56 -0400

    initial commit

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header siteTitle={"Jackson Isenberg"} />
       <div
         style={{
           margin: `0 auto`,
