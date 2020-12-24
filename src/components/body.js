ce8df50c204aa06b86221dac45d5d671339d30f3
JIceberg <jisenberg3@gatech.edu>
2020-12-24 06:42:52 -0500

    begin rewrite of site

--- src/components/body.js
+++ src/components/body.js
@@ -1,18 +1,11 @@
 import React from "react"
 import CustomLink from "../components/link"
-import resume from "../images/resume.png"
 
 export default function Body() {
     return (
         <p style={{ color: 'black', font: '400 15pt sans-serif', top: '0px' }}>
             My name is Jackson Isenberg, and I&#39;m a computer science major
             from the <CustomLink href="/" text="Georgia Institute of Technology" color="gold" />.
-            <br />
-            Here is my resume:
-            <br />
-            <a href={resume} download="jaxon-resume.png">
-                <img src={resume} />
-            </a>.
         </p>
     );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,15 @@ import React from "react"
 import Header from "../components/header"
 import Subtitle from "../components/subtitle"
 import Body from "../components/body"
+import { Helmet } from "react-helmet"
 
 function Home() {
     return (
         <div style={{ color: 'black', padding: '15px' }}>
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Jackson Isenberg</title>
+            </Helmet>
             <Header headerText="Jackson Isenberg"/>
             <Subtitle text="Welcome to my website!" />
             <Body />
