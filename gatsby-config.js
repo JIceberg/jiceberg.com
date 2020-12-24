984239cd8214212bcd274305d5f55a3ec9957791
JIceberg <jisenberg3@gatech.edu>
2020-12-24 06:59:54 -0500

    fix issues with deployment

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,8 +16,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `jiceberg-site`,
-        short_name: `jiceberg`,
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
--- src/components/body.js
+++ src/components/body.js
@@ -5,7 +5,11 @@ export default function Body() {
     return (
         <p style={{ color: 'black', font: '400 15pt sans-serif', top: '0px' }}>
             My name is Jackson Isenberg, and I&#39;m a computer science major
-            from the <CustomLink href="/" text="Georgia Institute of Technology" color="gold" />.
+            from the <CustomLink href="https://www.gatech.edu" text="Georgia Institute of Technology" color="gold" />.
+            I&#39;ve worked as project manager and lead developer for&nbsp;
+            <CustomLink href="https://www.github.com/FTCLib/FTCLib" text="FTCLib" color="#FFA500"/>
+            &nbsp;and creator of&nbsp;
+            <CustomLink href="http://www.jaxonfiles.com" text="The Jaxon Files" color="red"/>.
         </p>
     );
 }
